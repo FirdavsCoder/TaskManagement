fa59454ce1e084095eebe55f4a8c01b1230e254a
FirdavsCoder <firdavsyorkulov@gmail.com>
2024-02-01 02:47:26 +0500

    updated

---
+++ src/modules/users/user.module.js
@@ -0,0 +1,14 @@
+import {Router} from "express";
+import {UserController} from "./user.controller.js";
+import {UserService} from "./user.service.js";
+
+const router = Router()
+
+const userService = new UserService()
+const userController = new UserController(userService)
+
+router.post("/", userController.insert);
+router.get("/:id", userController.getById);
+router.put("/:id", userController.updateById);
+router.delete("/:id", userController.delete);
+router.get("/", userController.getAll);
