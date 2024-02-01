9d16892dbda20612ec898f08105ec77a1d6ea3ab
FirdavsCoder <firdavsyorkulov@gmail.com>
2024-02-02 00:44:25 +0500

    updated

---
+++ src/lib/roles.js
@@ -0,0 +1,11 @@
+export const roles = {
+	SUPERADMIN: "superAdmin",
+	ADMIN: "admin",
+	WORKER: "worker",
+	MANAGER: "manager",
+};
+
+export const status = {
+	PROCESS: "process",
+	DONE: "done",
+};
